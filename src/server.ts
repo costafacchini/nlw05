16594bd72b67fd989cb8154f0bbc3f5249e27a68
Alan Costa Facchini <costafacchini@gmail.com>
2021-04-19 18:03:58 -0300

    Aula 1

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá NLW 05' })
+});
+
+app.post('/', (request, response) => {
+  return response.json({ message: 'Usuário salvo com sucesso!' })
+})
+
+app.listen(3333, () => console.log('Server is run on port 3333'));
