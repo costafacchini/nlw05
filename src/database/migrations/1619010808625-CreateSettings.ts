07435f931ffc30f16a5b2708cde10cb6b18a2e38
Alan Costa Facchini <costafacchini@gmail.com>
2021-04-21 11:15:22 -0300

    Create Settings entity

---
+++ src/database/migrations/1619010808625-CreateSettings.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import {query} from "express";
+
+export class CreateSettings1619010808625 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "settings",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "username",
+                        type: "varchar"
+                    },
+                    {
+                        name: "chat",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "updatet_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("settings")
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
-import express from 'express';
-import './database'
+import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá NLW 05' })
-});
-
-app.post('/', (request, response) => {
-  return response.json({ message: 'Usuário salvo com sucesso!' })
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is run on port 3333'));
